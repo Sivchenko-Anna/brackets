6045d17c0989c3303db8597fc4ff2d907c687599
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-02-21 12:02:36 +1000

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let brakets = new Map(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+    const currentBracket = str[i];
+    if (currentBracket == brakets.get(stack[stack.length - 1])) {
+      stack.pop();
+    } else {
+      stack.push(currentBracket);
+    }
+  }
+  return stack.length === 0;
 }
